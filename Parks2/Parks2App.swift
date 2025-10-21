15c06db45397f5c724b900dcc9631c92ec54ef20
gnembhard <gnembbhard2020@fau.edu>
2025-10-21 10:18:37 -0400

    Initial Commit

---
+++ Parks2/Parks2App.swift
@@ -0,0 +1,17 @@
+//
+//  Parks2App.swift
+//  Parks2
+//
+//  Created by Giovanni Nembhard on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Parks2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Parks2Tests/Parks2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Parks2Tests.swift
+//  Parks2Tests
+//
+//  Created by Giovanni Nembhard on 10/21/25.
+//
+
+import Testing
+@testable import Parks2
+
+struct Parks2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
